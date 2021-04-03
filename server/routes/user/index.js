711c53a1cac64ba91ac6d9ce53ac8a557e5890c2
Andrew Wong <kkwong125@gmail.com>
2021-04-03 03:15:37 -0700

    hashed password before storing in db

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const pool = require("../../dbconfig");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
+const UsernameCheckQuery = "SELECT user_id FROM users WHERE username=$1";
 const UserInsertQuery =
   "INSERT INTO users (username, password) VALUES($1, $2) RETURNING *";
 
 router.post("/", async (req, res) => {
   try {
     const { name, password } = req.body;
-    const newUser = await pool.query(UserInsertQuery, [name, password]);
-    res.json(newUser.rows[0]);
-  } catch (err) {
-    if (err.code == 23505) {
+    const checkUser = await pool.query(UsernameCheckQuery, [name]);
+
+    if (checkUser.rowCount > 0) {
       res.json({ error: "Username not available" });
+    } else {
+      const hashedPw = await bcrypt.hash(password, saltRounds);
+      const newUser = await pool.query(UserInsertQuery, [name, hashedPw]);
+      res.json({ username: newUser.rows[0].username });
     }
+  } catch (err) {
+    res.json({ error: err.message });
     console.error(err);
   }
 });
