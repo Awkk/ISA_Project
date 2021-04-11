ac1eb416dae3ffc79ff6d8717c02e4a8b0ef4d95
Andrew Wong <kkwong125@gmail.com>
2021-04-10 23:10:18 -0700

    done

--- client/src/constant/api.js
+++ client/src/constant/api.js
@@ -1,2 +1,2 @@
-//export const baseurl = "https://isa-rebbit-server.herokuapp.com/api/v1";
-export const baseurl = "http://localhost:8888/api/v1";
+export const baseurl = "https://isa-rebbit-server.herokuapp.com/api/v1";
+//export const baseurl = "http://localhost:8888/api/v1";
